2b6c578529703756e6cf64e220a0123ace30f0d4
Jaylee Maruk <jmaruk@Jaylees-MBP.socal.rr.com>
2018-04-13 21:05:24 -0700

    add logic

--- logic.js
+++ logic.js
@@ -1,119 +1,66 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBR14f7vPtlDgWNoM0frtHQOoRLn9OOT_o",
+  authDomain: "traintimer-65445.firebaseapp.com",
+  databaseURL: "https://traintimer-65445.firebaseio.com",
+  projectId: "traintimer-65445",
+  storageBucket: "traintimer-65445.appspot.com",
+  messagingSenderId: "631098526381"
+};
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBzpeR_WPmiKeMs6SVeTx2RIQ-9ysYiKOY",
-    authDomain: "workouttracker-80449.firebaseapp.com",
-    databaseURL: "https://workouttracker-80449.firebaseio.com",
-    projectId: "workouttracker-80449",
-    storageBucket: "workouttracker-80449.appspot.com",
-    messagingSenderId: "463470735336"
-  };
- 
 
 firebase.initializeApp(config);
 
+$('#add-train').on('click', function() {
+var name = $('#name').val();
+var dest = $('#destination').val();
+var firstTime = $('#first-time').val();
+var freq = $('#frequency').val();
+
+
+var train = {
+  name: name,
+  destination: dest,
+  firstTime: firstTime,
+  frequency: freq,
+
+  dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+};
+
+console.log(train);
+
+var connectionsRef = trainDb.ref();
+
+connectionsRef.push(train);
+return false;
+
+});
+
 // Create a variable to reference the database.
-var database = firebase.database();
+var trainDb = firebase.database();
+
+
+
+trainDb.ref().orderByChild("dateAdded").limitToLast(2).on("child_added", function(snapshot) {
+
+// Change the HTML to reflect
+$("#train1").text(snapshot.val().name);
+$("#city1").text(snapshot.val().destination);
+$("#freq1").text(snapshot.val().frequency);
+$("#arrive").text(snapshot.val().firstTime);
+
+});
+
+
 
 // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
 // connectionsRef references a specific location in our database.
 // All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
+
 
 // '.info/connected' is a special location provided by Firebase that is updated every time
 // the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
 
 // When the client's connection state changes...
-connectedRef.on("value", function(snap) {
-
-  // If they are connected..
-  if (snap.val()) {
-
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
-
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
-
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snap) {
-
-  // Display the viewer count in the html.
-  // The number of online users is the number of children in the connections list.
-  $("#watchers").text(snap.numChildren());
-});
-
-// -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-// Set Initial Counter
-var initialValue = 100;
-var clickCounter = initialValue;
-
-// At the page load and subsequent value changes, get a snapshot of the local data.
-// This callback allows the page to stay updated with the values in firebase node "clicks"
-database.ref("/clicks").on("value", function(snapshot) {
-
-  // Print the local data to the console.
-  console.log(snapshot.val());
-
-
-  // Change the HTML to reflect the local value in firebase.
-  clickCounter = snapshot.val().clickCount;
-
-  // Log the value of the clickCounter
-  console.log(clickCounter);
-
-  // Change the HTML to reflect the local value in firebase.
-  $("#click-value").text(snapshot.val().clickCount);
-
-  // Change the HTML Value using a variable (similar to the above)
-  $("#click-value").text(clickCounter);
-
-// If any errors are experienced, log them to console.
-}, function(errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-// --------------------------------------------------------------
-
-// Whenever a user clicks the click-button
-$("#click-button").on("click", function() {
-
-  // Reduce the clickCounter by 1
-  clickCounter--;
-
-  // Alert User and reset the counter
-  if (clickCounter === 0) {
-    alert("Phew! You made it! That sure was a lot of clicking.");
-    clickCounter = initialValue;
-  }
-
-  // Save new value to Firebase
-  database.ref("/clicks").set({
-    clickCount: clickCounter
-  });
-
-  // Log the value of clickCounter
-  console.log(clickCounter);
-});
-
-// Whenever a user clicks the restart button
-$("#restart-button").on("click", function() {
-
-  // Set the clickCounter back to initialValue
-  clickCounter = initialValue;
-
-  // Save new value to Firebase
-  database.ref("/clicks").set({
-    clickCount: clickCounter
-  });
-
-  // Log the value of clickCounter
-  console.log(clickCounter);
-
-  // Change the HTML Values
-  $("#click-value").text(clickCounter);
-});
